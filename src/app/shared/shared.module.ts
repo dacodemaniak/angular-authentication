6fdeefae8d0e0aec61ed9258e111cb3b4a711cda
DaCodeManiaK <jla.webprojet@gmail.com>
2021-12-18 16:53:07 +0100

    Exports toolbars

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiModule } from './ui/ui.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    UiModule,
+    FlexLayoutModule
+  ],
+  exports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    UiModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
