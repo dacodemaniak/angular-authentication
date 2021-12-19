1547ffa7ac5a5cbafc67a3ea77590226f73cf5b6
DaCodeManiaK <jla.webprojet@gmail.com>
2021-12-19 16:43:00 +0100

    Implements logout event

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { StorageService } from '@services/storage.service';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +11,17 @@ export class HeaderComponent implements OnInit {
 
   public showBackArrow: boolean = true;
   
-  constructor() { }
+  constructor(
+    private storageService: StorageService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public logout(): void {
+    this.storageService.remove();
+    this.router.navigate(['/', 'account', 'signin']);
   }
 
 }
