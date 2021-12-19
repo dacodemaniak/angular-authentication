ce529d25bce8eca388ba276cc1fd9436e97594fe
DaCodeManiaK <jla.webprojet@gmail.com>
2021-12-19 08:29:32 +0100

    Protect home module loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IsAuthGuard } from './user/guards/is-auth.guard';
 
 @NgModule({
   imports: [RouterModule.forRoot(AppRoutingModule.routes)],
@@ -15,7 +16,8 @@ export class AppRoutingModule {
     {
       path: 'dashboard',
       loadChildren: () => import('./dashboard/dashboard.module')
-        .then((module) => module.DashboardModule)
+        .then((module) => module.DashboardModule),
+      canLoad: [IsAuthGuard]
     },
     {
       path: 'account',
