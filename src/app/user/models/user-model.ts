6aba88fc9c35d4a928f871ab6c6f4a237eac11a4
DaCodeManiaK <jla.webprojet@gmail.com>
2021-12-19 14:06:46 +0100

    Sets model and interface for user

--- src/app/user/models/user-model.ts
+++ src/app/user/models/user-model.ts
@@ -1,2 +1,8 @@
-export class UserModel {
+import { UserInterface } from "./user-interface";
+
+export class UserModel implements UserInterface {
+    public userName: string = '';
+    public userPass: string = '';
+    public roles?: string[] | undefined;
+    public token?: string | undefined;
 }
