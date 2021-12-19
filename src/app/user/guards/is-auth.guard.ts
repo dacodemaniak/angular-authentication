46072263c83cebf2d315a17639a9d6b7ac6ac451
DaCodeManiaK <jla.webprojet@gmail.com>
2021-12-19 08:29:16 +0100

    Add routing to signin after refactor

--- src/app/user/guards/is-auth.guard.ts
+++ src/app/user/guards/is-auth.guard.ts
@@ -1,19 +1,37 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { StorageService } from '@services/storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IsAuthGuard implements CanActivate, CanLoad {
+  public constructor(
+    private storageService: StorageService,
+    private router: Router
+  ) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      const isAuthorized: boolean = this.authorize();
+      if (!isAuthorized) {
+        this.router.navigate(['/', 'account']);
+      }
+      return isAuthorized;
   }
+  
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      const isAuthorized: boolean = this.authorize();
+      if (!isAuthorized) {
+        this.router.navigate(['/', 'account']);
+      }
+      return isAuthorized;
+  }
+
+  private authorize(): boolean {
+    return this.storageService.get() ? true : false;
   }
 }
